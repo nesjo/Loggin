5f2b6d28fa832f0cbafcf1b226235bc51333d45d
William Ventura <william.al1379@gmail.com>
2014-09-17 15:23:05 -0500

    Jquery and Bootstrap setup

---
+++ loggin.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+    <head>
+        <title>My Application PDO</title>
+        <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/bower_components/bootstrap/dist/css/bootstrap-theme.min.css">
+        <script src="assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+        <script src="assets/bower_components/jquery/dist/jquery.min.js"></script>
+        <style>
+            .jumbotron{
+                max-width: 400px;
+                margin: 0 auto;
+                margin-top: 50px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="jumbotron">
+                <form class="form-signin" role="form">
+                    <h2 class="form-signin-heading">Please sign in</h2>
+                    <input type="text" class="form-control" placeholder="Email address" required="" autofocus="">
+                    <input type="password" class="form-control" placeholder="Password" required="">
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
+
